6defbb0f97ed72997b9066deafd53db5cd396194
Benjamin Johnson <bjohnson@fcusd.org>
2024-09-09 19:34:04 -0700

    Update and rename Program3.py to Program4.py

---
+++ Program4.py
@@ -0,0 +1,64 @@
+## Problem 1 ##
+#Please write a script that: 
+# - Asks for 2 numbers
+# - Prints out those numbers dividing with int division and float division
+
+
+
+## Problem 2 ##
+#Please write a script that: 
+# - Asks user for their name and year of birth
+# - Prints something like:
+# Sample output:
+# --------------------------------------------------
+#  What is your name? Frances Fictitious
+#  Which year were you born? 1990
+#  Hi Frances Fictitious, you will be 31 years old at the end of the year 2021
+
+
+## Problem 3 ##
+#Please rewrite this script so that it only uses 2 variables. It should retain its original behavior 
+number1 = int(input("First number: "))
+number2 = int(input("Second number: "))
+number3 = int(input("Third number: "))
+
+sum = number1 + number2 + number3
+print(f"The sum of the numbers: {sum}")
+
+
+## Problem 4 ##
+#Please write a program that:
+# - Asks the user for a number of days. 
+# - Prints out the number of seconds in the amount of days given.
+
+
+
+## Problem 5 ##
+#Please write a program that:
+# - Asks the user for 4 numbers
+# - Prints out the sum and mean (average) of the numbers
+
+
+
+
+
+## Problem 6 ##
+#Please write a program that:
+# - Asks the user for 
+#   - Number of Students
+#   - Group size
+# - Prints out the number of groups formed using those inputs
+#   - If the division is not even, one of the groups may have fewer members than specified.
+# - Hint: the integer division operator // could come in handy here.
+# ---------------------------------------
+#Sample output
+#How many students on the course? 8
+#Desired group size? 4
+#Number of groups formed: 2
+#
+#Sample output
+#How many students on the course? 11
+#Desired group size? 3
+#Number of groups formed: 4
+#
+#
